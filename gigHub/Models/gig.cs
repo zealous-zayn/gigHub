82f28e17ebd5915785990b76c2c22e254f62a94d
zealous-zayn <imzayn93@gmail.com>
2019-05-02 22:53:44 +0530

    Gig Model added

---
+++ gigHub/Models/gig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gigHub.Models
+{
+    public class gig
+    {
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+    }
+
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
