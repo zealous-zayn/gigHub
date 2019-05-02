32cd065d5e575d47872d695c693234db38748cf3
zealous-zayn <imzayn93@gmail.com>
2019-05-02 22:43:13 +0530

    Add project files.

---
+++ gigHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gigHub.MvcApplication" Language="C#" %>
---
+++ gigHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gigHub.Startup))]
+namespace gigHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
