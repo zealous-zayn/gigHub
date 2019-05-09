a6eec5fb13954026b03a0c721f526c69544d54c4
Faisal Khan <imzayn93@gmail.com>
2019-05-09 13:58:00 +0530

    Some Html and Css

--- gigHub/App_Start/BundleConfig.cs
+++ gigHub/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace gigHub
 {
@@ -23,8 +22,8 @@ namespace gigHub
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"));
         }
     }
 }
